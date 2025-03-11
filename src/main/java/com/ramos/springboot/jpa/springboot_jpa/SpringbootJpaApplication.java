c204bf5f7fb62e32c1861e635fe45efac620f6a9
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-03-11 09:23:21 -0600

    Se crean registros en la BD con JPA

--- src/main/java/com/ramos/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/ramos/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -2,11 +2,13 @@ package com.ramos.springboot.jpa.springboot_jpa;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ramos.springboot.jpa.springboot_jpa.entities.Person;
 import com.ramos.springboot.jpa.springboot_jpa.repositories.PersonRepository;
@@ -23,9 +25,25 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		findOne();
+		create();
 	}
 
+	@Transactional
+	public void create(){
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		String lastname = sc.next();
+		String programmingLanguage = sc.next();
+		sc.close();
+
+		Person person = new Person(null, name, lastname, programmingLanguage); 
+
+		Person personNew = repository.save(person);
+		System.out.println(personNew);
+		repository.findById(personNew.getId()).ifPresent(p -> System.out.println(p));
+	}
+
+	@Transactional(readOnly = true)
 	public void findOne(){
 		Person person = null;
 		Optional<Person> optionalPerson = repository.findOneLikeName("In");
@@ -35,6 +53,7 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 		System.out.println(person);
 	}
 
+	@Transactional(readOnly = true)
 	public void list(){
 		//List<Person> persons = (List<Person>) repository.findAll();
 		//List<Person> persons = (List<Person>) repository.findByProgrammingLanguage("Java");
